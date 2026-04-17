34e5bd625705f6b8106a79ac4ca526e74d17903b
bokigolic <bokigolic33@gmail.com>
2026-04-16 22:38:44 -0400

    feat: React + Vite + PWA setup

---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'BeFluent',
+        short_name: 'BeFluent',
+        description: 'BeFluent - Language Learning App',
+        theme_color: '#09090f',
+        background_color: '#09090f',
+        display: 'standalone',
+        start_url: '/',
+        icons: [
+          {
+            src: '/icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ]
+})
